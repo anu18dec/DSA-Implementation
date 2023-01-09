8ea9428a6bcbdbf90fa68fee21f42e2545f1cd3e
anupam <dec18anu@gmail.com>
2023-01-10 00:06:54 +0530

    added base of linkedlist

---
+++ DoublyLinkedList.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+
+	public:
+		int Node;
+		Node * next;
+		Node * prev;
+
+};
+
+class DoublyList{
+	private:
+		Node * head;
+		Node * tail;
+
+	public:
+		DoublyList(){
+			head = NULL;
+			tail = NULL;
+		}
+
+		DoublyList(int size, int value = 0){
+			Node * temp;
+
+			if(size){
+				
+			}
+			while(size--){
+
+
+			}
+		}
+};
+
+int main()
+{
+	// Creating main for testing
+	
+	return 0;
+}
